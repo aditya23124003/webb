f59829ae59cd24fee6d21e31efbc0708e47f2583
Aditya Gupta <aditya.gupta.mat23@itbhu.ac.in>
2024-04-16 16:31:27 +0530

    have put some units like deegre celcius,etc

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-
+      
+      
       const apikey = "f8b372ea7fa00eda1d0c6514c74ba962";
       apiURL =
         `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
       const weatherIcon = document.querySelector("#weather-icon");
+
+
 
       async function checkWeather()
       {
@@ -17,13 +20,13 @@
 
           document.querySelector("#weather-description").innerHTML= data.weather[0].description;
           document.querySelector("#city").innerHTML = document.getElementById("city-search").value;
-          document.querySelector("#temp").innerHTML= Math.round(data.main.temp);
-          document.querySelector("#feels-like").innerHTML =Math.round (data.main.feels_like);
-          document.querySelector("#max-temp").innerHTML = (data.main.temp_max);
-          document.querySelector("#min-temp").innerHTML = (data.main.temp_min);
-          document.querySelector("#HUMIDITY").innerHTML = (data.main.humidity);
-          document.querySelector("#PRESSURE").innerHTML = (data.main.pressure);
-          document.querySelector("#visibility").innerHTML = (data.visibility);
+          document.querySelector("#temp").innerHTML="  "+ Math.round(data.main.temp)+"°"+"c".sup();
+          document.querySelector("#feels-like").innerHTML =Math.round (data.main.feels_like)+"°"+"c".sup();
+          document.querySelector("#max-temp").innerHTML = (data.main.temp_max)+"°"+"c".sup();
+          document.querySelector("#min-temp").innerHTML = (data.main.temp_min)+"°"+"c".sup();
+          document.querySelector("#HUMIDITY").innerHTML = (data.main.humidity)+"%";
+          document.querySelector("#PRESSURE").innerHTML = (data.main.pressure)+" hPa";
+          document.querySelector("#wind-speed").innerHTML = (data.wind.speed)+` miles/hr`;
 
           if(data.weather[0].main=="Clear")
           document.getElementById("weather-icon").src='images/clear.png';
@@ -48,20 +51,25 @@
 
           if(data.weather[0].main=="Wind")
           document.getElementById("weather-icon").src='images/wind.png';
+          const timeZone =data.timezone;
+          const sunriseUnix = data.sys.sunrise+timeZone;
+          const sunsetUnix = data.sys.sunset+timeZone;
 
-          const sunriseUnix = data.sys.sunrise;
-          const sunsetUnix = data.sys.sunset;
-
-function showSunTimes(sunriseUnix, sunsetUnix, timeZone) {
-    console.log( new Date(sunriseUnix * 1000).toLocaleString('default',{ timeZone }));
-    console.log( new Date(sunsetUnix * 1000).toLocaleString('default', { timeZone }));
+function showSunTimes(sunriseUnix, sunsetUnix, timeZone) 
+{
+    document.querySelector("#sunrise").innerHTML=( new Date(sunriseUnix * 1000).toLocaleString('default',{ timeZone })).slice(12,17);
+    document.querySelector("#sunset").innerHTML=( new Date(sunsetUnix * 1000).toLocaleString('default', { timeZone })).slice(12,17);
+    document.querySelector("#date").innerHTML =( new Date(sunriseUnix * 1000).toLocaleString('default',{ timeZone })).slice(0,10);
+    
 }
 
-showSunTimes(sunriseUnix, sunsetUnix,UTC );
-showSunTimes(sunriseUnix, sunsetUnix, UTC); 
+showSunTimes(sunriseUnix, sunsetUnix,"UTC" );
+
          
 
 
-      }
+}
+
+
 
     
